2767943c39dd8ab38fe729bd60f981d16c84cb74
javier198191 <javierlasprilla1@gmail.com>
2026-03-25 01:39:31 -0500

    infra: docker postgres activo y base de nestjs mapeada (fix pendiente en dto)

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   
   // Registrar el filtro de excepciones global de Prisma
   app.useGlobalFilters(new PrismaExceptionFilter());
